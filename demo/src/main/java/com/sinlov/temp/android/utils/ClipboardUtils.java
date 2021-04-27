fa1b74e99bdac59b720bf8a6a7e09fd3a68e4c15
sinlov <sinlovgmppt@gmail.com>
2021-04-27 14:36:27 +0800

    update ClipboardUtils.java

--- demo/src/main/java/com/sinlov/temp/android/utils/ClipboardUtils.java
+++ demo/src/main/java/com/sinlov/temp/android/utils/ClipboardUtils.java
@@ -1,18 +1,51 @@
 package com.sinlov.temp.android.utils;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.content.ClipboardManager;
 import android.content.Context;
 
-public class ClipboardUtils {
+import androidx.annotation.IntRange;
+import androidx.annotation.NonNull;
 
-    public static void copy2Clipboard(Context context, String content) {
+public final class ClipboardUtils {
+
+    public static void copy2Clipboard(@NonNull Context context, @NonNull String content) {
         content = content.trim();
         ClipboardManager cmb = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
-        cmb.setText(content.trim());
+        ClipData clipData = ClipData.newPlainText(content.trim(), content.trim());
+        cmb.setPrimaryClip(clipData);
     }
 
-    public static String paste(Context context) {
+    public static CharSequence item(@NonNull Context context, @IntRange(from = 0, to = 10) int count) {
         ClipboardManager cmb = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
-        return cmb.getText().toString().trim();
+        ClipData primaryClip = cmb.getPrimaryClip();
+        if (primaryClip == null) {
+            return "";
+        }
+        if (primaryClip.getItemCount() < count) {
+            return "";
+        }
+        return primaryClip.getItemAt(count).getText();
+    }
+
+    public static String label(@NonNull Context context) {
+        ClipboardManager cmb = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData primaryClip = cmb.getPrimaryClip();
+        if (primaryClip == null) {
+            return "";
+        }
+        ClipDescription description = primaryClip.getDescription();
+        if (description == null) {
+            return "";
+        }
+        CharSequence label = description.getLabel();
+        if (label == null) {
+            return "";
+        }
+        return label.toString();
+    }
+
+    private ClipboardUtils() {
     }
 }
