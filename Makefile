d83107ad271a71ae0544f242ed0d93879901fc54
sinlov <sinlovgmppt@gmail.com>
2024-11-09 08:29:12 +0800

    build: add common make tasks and improve help
    - Add .PHONY target for help
    - Implement helpAndroidBase target
    - Add init, cleanRoot, cleanGradleBuildAndIdea, and adbCrash targets
    - Update help target to include new sub-help targets

--- Makefile
+++ Makefile
@@ -6,5 +6,6 @@ include z-android-base.mk
 include plugin/z-plugin.mk
 include demo/z-demo.mk
 
+.PHONY: help
 help: helpAndroidBase help-plugin help-demo
 	@echo "more task see Makefile!"
--- z-android-base.mk
+++ z-android-base.mk
@@ -6,18 +6,23 @@ ifndef ANDROID_HOME
 endif
 
 # init this project
+.PHONY: init
 init:
 	@$(ROOT_PWD)/gradlew clean buildEnvironment --warning-mode all
 
+.PHONY: cleanRoot
 cleanRoot:
 	$(ROOT_PWD)/gradlew clean
 
+.PHONY: cleanGradleBuildAndIdea
 cleanGradleBuildAndIdea:
 	$(ROOT_PWD)/gradlew clean cleanBuildCache cleanIdea
 
+.PHONY: adbCrash
 adbCrash: checkEnvAndroidHome
 	adb shell dumpsys dropbox --print data_app_crash
 
+.PHONY: helpAndroidBase
 helpAndroidBase:
 	@echo "=> $(ROOT_PWD)/z-android-base.mk : android base task"
 	@echo "make init                    ~> init this project for check base build error"
